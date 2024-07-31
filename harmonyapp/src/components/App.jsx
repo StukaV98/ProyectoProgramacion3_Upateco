763d49e49560a6d234967a203e50e075e534fd6d
Walter Agustín <101841775+WalterJr10@users.noreply.github.com>
2024-07-30 21:27:32 -0300

    create app&footer

---
+++ harmonyapp/src/components/App.jsx
@@ -0,0 +1,18 @@
+import Componente from "./Componente";
+import Artista from "./Artista";
+import Footer from "./Footer";
+
+function App() {
+
+  return (
+    <div>
+      <Componente></Componente>
+      <h1>Welcome to app music</h1>
+      <Artista></Artista>
+      <footer><Footer></Footer></footer>
+    </div>
+  )
+
+}
+
+export default App;
