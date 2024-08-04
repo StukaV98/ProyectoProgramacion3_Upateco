3df55df148c5b71a7ba58c7c22f07e70467d1962
Walter Agustín <101841775+WalterJr10@users.noreply.github.com>
2024-08-04 13:44:36 -0300

    useFetchjs

--- harmonyapp/src/components/hooks/useFetch.js
+++ harmonyapp/src/components/hooks/useFetch.js
@@ -1,1 +1,36 @@
+import { useEffect, useState } from 'react'
 
+export default function useFetch(url, options = {}, errorMessagge= 'Error de peticion') {
+
+    const [data, setData] = useState(null);
+    const [isError, setIsError] = useState(false);
+    const [loading, setLoading] = useState(true);
+    
+
+    useEffect(() => {
+        setData(null)
+        setIsError(false)
+        setLoading(true)
+
+        fetch(url,{...options})
+            .then((response) => {
+                if(response.ok){
+                    return response.json()
+                }
+            })
+            .then((data) => {
+                setData(data)
+            })
+            .catch((e) => {
+                setIsError(true)
+            })
+            .finally(()=>{
+                setLoading(false)
+            })
+
+
+    },[url])
+
+
+    return [data, isError, loading]
+}
