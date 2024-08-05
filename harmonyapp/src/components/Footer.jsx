b773348bf94c96df4abc8e7850ef4788adb853e8
StukaV98 <lucas_emiliano_2009@hotmail.com>
2024-08-05 17:17:20 -0300

    Prueba

---
+++ harmonyapp/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <p>Todos los derechos reservados - Grupo programacion {new Date().getFullYear()}</p>
+    </div>
+  )
+}
+
+export default Footer
