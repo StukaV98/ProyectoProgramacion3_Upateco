d512aa0e7f86e9750c4921d058861d49f4aba382
StukaV98 <lucas_emiliano_2009@hotmail.com>
2024-08-06 19:31:24 -0300

    Reestructuración general del proyecto

--- harmonyapp/src/components/App.jsx
+++ harmonyapp/src/components/App.jsx
@@ -1,12 +1,14 @@
 import Componente from "./Componente";
 import Artista from "./Artista";
 import Footer from "./Footer";
+import Login from "./Login";
 
 function App() {
 
   return (
     <div>
       <Componente></Componente>
+      <Login/>
       <h1>Welcome to app music</h1>
       <Artista></Artista>
       <footer><Footer></Footer></footer>
--- harmonyapp/src/main.jsx
+++ harmonyapp/src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './components/App.jsx'
+import Login from './components/Login.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
