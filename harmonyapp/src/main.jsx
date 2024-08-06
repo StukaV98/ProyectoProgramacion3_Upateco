3e037c984820d8848e0669fea1785d5319f678e0
Walter Agustín <101841775+WalterJr10@users.noreply.github.com>
2024-08-05 21:36:45 -0300

    main enrutador prueba
    main router provider

--- harmonyapp/src/main.jsx
+++ harmonyapp/src/main.jsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import Router from './components/routes/Router.jsx';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={Router} />
+);
 
-)
