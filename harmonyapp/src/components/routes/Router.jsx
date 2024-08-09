a584e78bb0970934bef2aa94d11e6673c66c4fa9
Walter Agustín <101841775+WalterJr10@users.noreply.github.com>
2024-08-09 18:19:57 -0300

    Router version2
    etapa ultimos detalles de router

--- harmonyapp/src/components/routes/Router.jsx
+++ harmonyapp/src/components/routes/Router.jsx
@@ -2,29 +2,89 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home";
 import Artista from "../Artista";
 import Contacto from "../Contacto";
+import Profile from "../Profile";
 import Login from "../Login";
+import Layout from "./Layout";
+import ProtectedRoute from './ProtectedRoute'
+import Playlists from "../Playlists";
+import Songs from "../Songs";
+import SongCreate from "../SongCreate";
 
-/* enrutador version 1 */
+
 const Router = createBrowserRouter([
     {
-        path: "/",
-        element: <Home/>
-    },
-    {
-        path: "/artista",
-        element: <Artista/>
-    },
-    {
-        path: "/contacto",
-        element: <Contacto/>
-    },
-    {
-        path: "/login",
-        element: <Login/>
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                children: [
+                    {
+                        index: true,
+                        element: <Home />,
+                    },
+                ]
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/contacto",
+                element: <Contacto />,
+            },
+            {
+                path: "/profile",
+                element: (
+                    <ProtectedRoute>
+                        <Profile />
+                    </ProtectedRoute>
+                ),
+            },
+            {
+                path: "/playlists",
+                element: (
+                    <ProtectedRoute>
+                        <Playlists />
+                    </ProtectedRoute>
+                )
+            },
+            {
+                path: "/artistas",
+                element: (
+                    <ProtectedRoute>
+                        <Artista />
+                    </ProtectedRoute>
+                )
+            },
+            {
+                path: "/songs",
+                children: [
+                    {
+                        index: true,
+                        element: (
+                            (
+                                <ProtectedRoute>
+                                    <Songs />
+                                </ProtectedRoute>
+                            )
+                        ),
+                    },
+                    {
+                        path: "new",
+                        element: (
+                                <ProtectedRoute>
+                                    <SongCreate />
+                                </ProtectedRoute>
+                        )
+                    },
+                ],
+            },
+
+        ]
     },
     {
         path: "*",
-        element: <h2>Pagina no encontrada</h2>
+        element: <h2>Pagina no encontrada</h2>,
     },
 ])
 
