4d308f65d810020cdff98ded8b82fd2cdaef3117
StukaV98 <lucas_emiliano_2009@hotmail.com>
2024-08-11 11:31:41 -0300

    Estilos 2

--- harmonyapp/src/components/PlaylistCard.jsx
+++ harmonyapp/src/components/PlaylistCard.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export const PlaylistCard = ({ playlist, userId }) => {
+  const { name, description, public: isPublic, entries, owner } = playlist;
 
-    const { name, description, public: isPublic, entries, owner } = playlist;
+  return (
+    <div className="card">
+      <header className="card-header">
+        <p className="card-header-title">{name}</p>
+      </header>
+      <div className="card-content">
+        <div className="content">   
 
-    return (
-        <div>
-            <div>
-                <h3>{name}</h3>
-                <p>Descripción: <span>{description ? description : "No especificada"}</span></p>
-                <p>Pública: <span>{isPublic ? "Sí" : "No"}</span></p>
-                <p>Canciones: <span>{entries ? entries : "No especificado"}</span></p>
-            </div>
+          <p><strong>Descripción:</strong> {description || "No especificada"}</p>
+          <p><strong>Pública:</strong> {isPublic ? "Sí" : "No"}</p>
+          <p><strong>Canciones:</strong> {entries || "No especificadas"}</p>
+          {userId === owner && (
+            <button className="button is-small is-primary">Editar</button>
+          )}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
--- harmonyapp/src/components/Playlists.jsx
+++ harmonyapp/src/components/Playlists.jsx
@@ -4,113 +4,117 @@ import { PlaylistCard } from './PlaylistCard';
 import { useAuth } from './contexts/AuthContext';
 
 export default function Playlists() {
-    const [playlists, setPlaylists] = useState([]);
-    const [nextURL, setNextURL] = useState(null);
-    const [isError, setIsError] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [pagina, setPagina] = useState(1);
+  const [playlists, setPlaylists] = useState([]);
+  const [nextURL, setNextURL] = useState(null);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [pagina, setPagina] = useState(1);
 
-    const { token, user__id } = useAuth("state");
+  const { token, user__id } = useAuth("state");
 
-    let url = `${import.meta.env.VITE_API_BASE_URL}/harmonyhub/playlists/?page=${pagina}`;
-    let paginaOwner = `${import.meta.env.VITE_API_BASE_URL}/harmonyhub/playlists/?owner=${user__id}`;
+  let url = `${import.meta.env.VITE_API_BASE_URL}/harmonyhub/playlists/?page=${pagina}`;
+  let paginaOwner = `${import.meta.env.VITE_API_BASE_URL}/harmonyhub/playlists/?owner=${user__id}`;
 
-    /*
-        Funcion asincrona que 
-    */
-
-    const doFetch = async () => {
-        setIsLoading(true);
-        fetch(url, {
-            headers: {
-                Authorization: `Token ${token}`,
-            },
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Solicitud a las playlists errónea');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                if (data.results) {
-                    setPlaylists((prevPlaylists) => [...prevPlaylists, ...data.results]);
-                    setNextURL(data.next);
-                }
-            })
-            .catch((error) => {
-                setIsError(true);
-                console.error(error, 'Error con el Data.');
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    };
-
-    function handleMyPlaylists() {
-        setPlaylists([]);
-        setIsLoading(true);
-        fetch(paginaOwner, {
-            headers: {
-                Authorization: `Token ${token}`,
-            },
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Solicitud a las playlists errónea');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                if (data.results) {
-                    setPlaylists(data.results);
-                }
-            })
-            .catch((error) => {
-                setIsError(true);
-                console.error(error, 'Error con mis playlists.');
-            })
-            .finally(() => {
-                setIsLoading(false);
-            });
-    }
-
-    function handleCargar() {
-        if (nextURL) {
-            setPagina((currentPage) => currentPage + 1);
+  const doFetch = async () => {
+    setIsLoading(true);
+    fetch(url, {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Solicitud a las playlists errónea');
         }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.results) {
+          setPlaylists((prevPlaylists) => [...prevPlaylists, ...data.results]);
+          setNextURL(data.next);
+        }
+      })
+      .catch((error) => {
+        setIsError(true);
+        console.error(error, 'Error con el Data.');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  function handleMyPlaylists() {
+    setPlaylists([]);
+    setIsLoading(true);
+    fetch(paginaOwner, {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Solicitud a las playlists errónea');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.results) {
+          setPlaylists(data.results);
+        }
+      })
+      .catch((error) => {
+        setIsError(true);
+        console.error(error, 'Error con mis playlists.');
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
+  function handleCargar() {
+    if (nextURL) {
+      setPagina((currentPage) => currentPage + 1);
     }
+  }
 
-    useEffect(() => {
-        doFetch();
-    }, [pagina]);
+  useEffect(() => {
+    doFetch();
+  }, [pagina]);
 
-    return (
-        <div>
-            <header>
-                <a href="/profile">Mi perfil</a>
-                <a href="/songs">Canciones</a>
-                <a href="/artistas">Artistas</a>
-            </header>
-            <h1>Playlists</h1>
+  return (
+    <div className="section">
+      <div className="container">
+        <header className="navigation is-horizontal">
+          <a href="/profile" className="navigation-item has-text-black">
+            Mi perfil
+          </a>
+          <a href="/songs" className="navigation-item has-text-black">
+            Canciones
+          </a>
+          <a href="/artistas" className="navigation-item has-text-black">
+            Artistas
+          </a>
+        </header>
+        <h1 className="title is-2">Playlists</h1>
 
-            {isLoading && <p>Cargando playlists...</p>}
-            <ul>
-                {playlists.map((playlist) => (
-                    <li key={playlist.id}>
-                        <PlaylistCard playlist={playlist} userId={user__id} />
-                    </li>
-                ))}
-            </ul>
-            <div>
-                {isLoading && <p>Cargando más playlists...</p>}
-                {nextURL && !isLoading && (
-                    <button onClick={handleCargar}>
-                        Cargar más
-                    </button>
-                )}
-            </div>
-            <Footer />
+        {isLoading && <p className="has-text-centered">Cargando playlists...</p>}
+        <ul className="list">
+          {playlists.map((playlist) => (
+            <li key={playlist.id} className="list-item">
+              <PlaylistCard playlist={playlist} userId={user__id} />
+            </li>
+          ))}
+        </ul>
+        <div className="has-text-centered">
+          {isLoading && <p>Cargando más playlists...</p>}
+          {nextURL && !isLoading && (
+            <button onClick={handleCargar} className="button is-primary">
+              Cargar más
+            </button>
+          )}
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 }
