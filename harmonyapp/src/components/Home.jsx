1904be039e5a80f887d5c5f02b22ee1fb5de8c93
Walter Agustín <101841775+WalterJr10@users.noreply.github.com>
2024-08-11 11:59:52 -0300

    home-estilos-fin

--- harmonyapp/src/components/Home.jsx
+++ harmonyapp/src/components/Home.jsx
@@ -1,34 +1,42 @@
-import React from 'react';
-import Componente from './Componente';
+import React from 'react'
 import Footer from './Footer';
 import { useAuth } from './contexts/AuthContext';
 
-function Home() {
-    return (
-        <div className="container">
-            <header className="box has-background-primary-light has-text-centered">
-                <Componente />
-            </header>
 
+
+function Home() {
+
+    const urlApi = `${import.meta.env.VITE_API_BASE_URL}/docs/`
+
+    return (
+        <div>
+            <div className='box has-text-centered'>
+                <nav className="navbar-menu is-active grid">
+                    <ul className='navbar-brand'>
+                        <li className='navbar-item'><a className='button is-link is-dark is-outlined' href={urlApi}>Api</a></li>
+                        <li className='navbar-item'><a className='button is-link is-dark is-outlined' href="/contacto">Contacto</a></li>
+                        <li className='navbar-item'><a className='button is-primary is-dark is-outlined' href="/login">Iniciar sesion</a></li>
+                    </ul>
+                </nav>
+            </div>
             <section className="section has-text-centered">
-                <h1 className="title has-text-primary-white">Welcome to music app</h1>
+                <h1 className="title has-text-primary-white" >Welcome to music app</h1>
                 
-                <div className="content">
-                    <h3 className="subtitle has-text-info-light">En esta aplicación se encuentran Playlists, Artistas y Canciones</h3>
+                <div className='content'>
+                    <h3 className="subtitle has-text-info-light">En esta aplicacion se encuentran Playlists, Artistas y Canciones</h3>
                     <p className="has-text-grey-light">Para crear artistas y canciones deberás iniciar sesión</p>
                     <p className="has-text-grey-light">Navega libremente por la web</p>
-                </div>
+                    <div className="buttons is-centered mt-5">
+                        <a className='button is-link' href="/playlists">Ir a Playlists</a>
+                        <a className='button is-link' href="/artistas">Ir a Artistas</a>
+                        <a className='button is-link' href="/songs">Ir a Canciones</a>
 
-                <div className="buttons is-centered mt-5">
-                    <a href="/playlists" className="button is-info">Ir a Playlists</a>
-                    <a href="/artistas" className="button is-primary">Ir a Artistas</a>
-                    <a href="/songs" className="button is-success">Ir a Canciones</a>
+                    </div>
                 </div>
             </section>
-
             <Footer />
         </div>
-    );
+    )
 }
 
-export default Home;
+export default Home
