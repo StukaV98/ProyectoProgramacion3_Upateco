5ee1a918556f6d34275813e4db8a864738157c1a
StukaV98 <lucas_emiliano_2009@hotmail.com>
2024-08-10 23:36:24 -0300

    Manejo de Estilos

--- harmonyapp/src/components/Contacto.jsx
+++ harmonyapp/src/components/Contacto.jsx
@@ -2,24 +2,24 @@ import React from 'react';
 
 function Contacto() {
     return (
-        <div className="box">
-            <h1 className="title is-2">Contacto</h1>
-            <p className="is-size-5">
+        <div className="box has-text-centered has-background-primary-light">
+            <h1 className="title is-2 has-text-info">Contacto</h1>
+            <p className="is-size-5 has-text-warning-dark">
                ¡Seguinos en nuestras redes!
             </p>
-            <div className="social-media">
+            <div className="buttons is-centered mt-4">
                 <a
-                    href="https://facebook.com"
+                    href="https://www.facebook.com/UPATECOSalta"
                     target="_blank"
-                    className="button is-light is-large"
+                    className="button is-link has-text-white-bis has-background-info-dark"
                     rel="noopener noreferrer"
                 >
                     Facebook
                 </a>
                 <a
-                    href="https://instagram.com"
+                    href="https://www.instagram.com/upateco_universidad/"
                     target="_blank"
-                    className="button is-light is-large"
+                    className="button has-text-white-bis has-background-success-dark"
                     rel="noopener noreferrer"
                 >
                     Instagram
--- harmonyapp/src/main.jsx
+++ harmonyapp/src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'; 
 import Router from './components/routes/Router';
 import { RouterProvider } from "react-router-dom";
+import 'bulma/css/bulma.min.css';
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
